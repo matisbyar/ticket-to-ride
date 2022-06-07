180cbd6a18d76199d8c307c52f71711798a1f50a
byarm <matis.byar@etu.umontpellier.fr>
2022-06-07 20:42:05 +0200

    MÀJ : Ajout d'un bouton qui masque certains éléments à la demande du joueur Notes : Avancée : 7 juin 2022 20:41

--- src/main/java/fr/umontpellier/iut/vues/panneauBas/VueReglages.java
+++ src/main/java/fr/umontpellier/iut/vues/panneauBas/VueReglages.java
@@ -1,26 +1,30 @@
 package fr.umontpellier.iut.vues.panneauBas;
 
+import fr.umontpellier.iut.vues.VueDuJeu;
 import fr.umontpellier.iut.vues.VuesHorsJeu.VueRegles;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 
 public class VueReglages extends HBox {
 
     private Button pause, regles, logs;
+    private ToggleButton masquer;
     private VueRegles vueRegles;
 
     public VueReglages() {
         pause = new Button("Pause");
         regles = new Button("Règles");
         logs = new Button("Log");
+        masquer = new ToggleButton("Carte");
 
         vueRegles = new VueRegles();
 
         creerBindings();
 
-        this.getChildren().addAll(pause, regles, logs);
+        this.getChildren().addAll(pause, regles, logs, masquer);
 
         styliser();
     }
@@ -28,6 +32,14 @@ public class VueReglages extends HBox {
     public void creerBindings() {
         regles.setOnAction(click -> {
             vueRegles.show();
+        });
+
+        masquer.setOnAction(click -> {
+            if (masquer.isSelected()) {
+                ((VueDuJeu) getScene().getRoot()).masquer();
+            } else {
+                ((VueDuJeu) getScene().getRoot()).afficher();
+            }
         });
     }
 
